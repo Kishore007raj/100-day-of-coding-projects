0a97de441081a05ce1891d93fe62f9f142625c97
M.Kishoreraj <141572538+Kishore007raj@users.noreply.github.com>
2024-06-18 11:45:20 +0530

    Update data.py

--- 100-days-of-code/final projects/day-16/data.py
+++ 100-days-of-code/final projects/day-16/data.py
@@ -8,6 +8,8 @@ question_data = [
     {"text": "It is illegal to pee in the Ocean in Portugal.", "answer": "True"},
     {"text": "You can lead a cow down stairs but not up stairs.", "answer": "False"},
     {"text": "Google was originally called 'Backrub'.", "answer": "True"},
+    {"text": "Liverpool is the 2019 ucl winners", "answer": "True"},
+    {"text": "Manchester City is the worst team in football history", "answer": "True"},
     {"text": "Buzz Aldrin's mother's maiden name was 'Moon'.", "answer": "True"},
     {"text": "No piece of square dry paper can be folded in half more than 7 times.", "answer": "False"},
     {"text": "A few ounces of chocolate can to kill a small dog.", "answer": "True"},
